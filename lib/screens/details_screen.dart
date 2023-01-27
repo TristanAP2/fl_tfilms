4e58daa2c133425664becda92b7dd4bd270e4ddc
Tristan Alonso <tristanalonso09@gmail.com>
2023-01-27 12:16:42 +0100

    modificado details y movieSlider

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,11 +6,93 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+      // Un tipo de appbar
+      body: CustomScrollView(
+        //Para que funcoine el widget tenemos que ponerle el sliver (como un childen pero solo admiten widgets especiales)
+        slivers: [
+          _CustomAppBar(),
+          SliverList(delegate: SliverChildListDelegate([
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: FadeInImage(
+                          placeholder: AssetImage('assets/no-image.jpg'), 
+                          image: NetworkImage('https://avatar.amuniversal.com/user_avatars/avatars_gocomicsver3/1241000/1241766/spongebob-300x400.jpg'),
+                          width: 125,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Titulo',
+                            style: Theme.of(context).textTheme.headline5,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                          Text('Subtitulo',
+                            style: Theme.of(context).textTheme.subtitle1,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star_half),
+                            ],
+                          )
+                        ],
+                      )
+                      
+                    ],
+                  ),
+                ),
+            ])
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      
+      //Comportamiento al hacer scroll
+      floating: true,
+      pinned: true,
+      snap: true,
+
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black38,
+          child: Text('movie.title', style: TextStyle(fontSize: 16),),
         ),
+        background: FadeInImage(
+                placeholder: AssetImage('assets/loading.gif'), 
+                image: NetworkImage('https://avatar.amuniversal.com/user_avatars/avatars_gocomicsver3/1241000/1241766/spongebob-300x400.jpg'),
+                fit: BoxFit.cover,
+              ),
       ),
+
     );
   }
 }
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -8,7 +8,6 @@ class MovieSlider extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 350,
-      color: Colors.red,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -27,16 +26,60 @@ class MovieSlider extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemCount: 20,
               itemBuilder: (context, index) {
-                return Container(
-                  width: 130,
-                  height: 190,
-                  color: Colors.green,
-                  margin: const EdgeInsets.all(10),
-                );
+                return _MoviePoster();
               },
             ),
           )
         ]
+      ),
+    );
+  }
+}
+
+// Barra baja para indicar que es una clase PRIVADA
+class _MoviePoster extends StatelessWidget {
+  const _MoviePoster({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 190,
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          //Poster
+          GestureDetector(
+            onTap: () => Navigator.popAndPushNamed(
+              context, 'details', 
+              arguments: 'movie-instance'
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://avatar.amuniversal.com/user_avatars/avatars_gocomicsver3/1241000/1241766/spongebob-300x400.jpg'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          SizedBox( height: 10,),
+          //Titulo
+          Text(
+            'El Señor de los Anillos: El Retorno del Rey',
+            //Acortar el texto a dos lineas
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            
+          )
+
+        ],
       ),
     );
   }
